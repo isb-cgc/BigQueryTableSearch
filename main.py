e23b19980747afc4a83bde0637c12e0c48196a8b
eleeisb <elee@systemsbiology.org>
2025-02-03 17:40:24 -0800

    tooltip text and icon for Always Newest filter updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ###
-# Copyright 2023, ISB
+# Copyright 2025, ISB
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,69 +16,46 @@
 
 import os
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_talisman import Talisman
+# from flask_talisman import Talisman
 import requests
 from google.cloud import bigquery, logging
-from datetime import datetime
-from random import randint
+from google.api_core.exceptions import BadRequest
+# from datetime import datetime
+# from random import randint
+import bq_builder
+import settings
 import re
-
+import concurrent.futures
+import json
 
 app = Flask(__name__)
-TIER = os.environ.get('TIER', 'test')
-app.config['TESTING'] = (TIER.lower() != 'prod')
-app.config['ENV'] = 'production' if TIER.lower() == 'prod' else 'development'
+# TIER = os.environ.get('TIER', 'test')
+# app.config['TESTING'] = (TIER.lower() != 'prod')
+# app.config['ENV'] = 'production' if TIER.lower() == 'prod' else 'development'
 
 # length of time (in seconds) the browser will respect the HSTS header
 # production and UAT should be set to 31,536,000 seconds (by not setting any HSTS_MAX_age,
 # else set to 3600 (test and dev)
-hsts_max_age = int(os.environ.get('HSTS_MAX_AGE') or 3600)
+# hsts_max_age = int(os.environ.get('HSTS_MAX_AGE') or 3600)
 
-Talisman(app, strict_transport_security_max_age=hsts_max_age, content_security_policy={
-    'default-src': [
-        '\'self\'',
-        '*.googletagmanager.com',
-        '*.google-analytics.com',
-        '*.googleapis.com',
-        "*.fontawesome.com",
-        '*.jsdelivr.net',
-        '\'unsafe-inline\'',
-        'data:',
-        'blob:'
-    ]
-})
+# Talisman(app, strict_transport_security_max_age=hsts_max_age, content_security_policy={
+#     'default-src': [
+#         '\'self\'',
+#         '*.googletagmanager.com',
+#         '*.google-analytics.com',
+#         '*.googleapis.com',
+#         "*.fontawesome.com",
+#         '*.jsdelivr.net',
+#         '\'unsafe-inline\'',
+#         'data:',
+#         'blob:'
+#     ]
+# })
 
 if os.environ.get('IS_GAE_DEPLOYMENT', 'False') != 'True':
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(app.root_path, 'privatekey.json')
 logging_client = logging.Client()
-bq_total_entries = 0
-BQ_ECOSYS_BUCKET = os.environ.get('BQ_ECOSYS_BUCKET',
-                                  'https://storage.googleapis.com/webapp-static-files-isb-cgc-dev/bq_ecosys/')
-# BQ_FILTER_FILE_NAME = 'bq_meta_filters_test.json'
-BQ_FILTER_FILE_NAME = 'bq_meta_filters.json'
-BQ_FILTER_FILE_PATH = BQ_ECOSYS_BUCKET + BQ_FILTER_FILE_NAME
-# BQ_METADATA_FILE_NAME = 'bq_meta_data_test.json'
-BQ_METADATA_FILE_NAME = 'bq_meta_data.json'
-BQ_METADATA_FILE_PATH = BQ_ECOSYS_BUCKET + BQ_METADATA_FILE_NAME
-BQ_USEFUL_JOIN_FILE_NAME = 'bq_useful_join.json'
-BQ_USEFUL_JOIN_FILE_PATH = BQ_ECOSYS_BUCKET + BQ_USEFUL_JOIN_FILE_NAME
-# BQ_VERSIONS_FILE_NAME = 'bq_versions_test.json'
-BQ_VERSIONS_FILE_NAME = 'bq_versions.json'
-BQ_VERSIONS_FILE_PATH = BQ_ECOSYS_BUCKET + BQ_VERSIONS_FILE_NAME
-BQ_MARKED_TABLE_MAP_FILE_NAME = 'bq_marked_tbl_map.json'
-BQ_MARKED_TABLE_MAP_FILE_PATH = BQ_ECOSYS_BUCKET + BQ_MARKED_TABLE_MAP_FILE_NAME
-
-bq_table_files = {
-    'bq_filters': {'last_modified': None, 'file_path': BQ_FILTER_FILE_PATH,
-                   'file_data': None},
-    'bq_metadata': {'last_modified': None, 'file_path': BQ_METADATA_FILE_PATH,
-                    'file_data': None},
-    'bq_useful_join': {'last_modified': None, 'file_path': BQ_USEFUL_JOIN_FILE_PATH,
-                       'file_data': None},
-    'bq_versions': {'last_modified': None, 'file_path': BQ_VERSIONS_FILE_PATH,
-                    'file_data': None},
-    'bq_marked_table_map': {'last_modified': None, 'file_path': BQ_MARKED_TABLE_MAP_FILE_PATH, 'file_data': None}
-}
+# bq_total_entries = 0
 
 
 @app.route("/", methods=['POST', 'GET'])
@@ -103,7 +80,10 @@ def privacy():
 
 @app.route("/search", methods=['POST', 'GET'])
 def search(status=None):
-    setup_app()
+    # settings.setup_app()
+    error_msg = settings.pull_metadata()
+    if error_msg:
+        app.logger.error(f"[ERROR] {error_msg}")
     selected_filters = {}
     if request.method == 'POST':
         rq_meth = request.form
@@ -114,17 +94,40 @@ def search(status=None):
               'reference_genome']:
         if rq_meth.get(f):
             selected_filters[f] = request.args.get(f).lower()
-    return render_template("bq_meta_search.html", bq_filters=bq_table_files['bq_filters']['file_data'],
+    return render_template("bq_meta_search.html", bq_filters=settings.bq_table_files['bq_filters']['file_data'],
                            selected_filters=selected_filters,
-                           bq_total_entries=bq_total_entries)
+                           bq_total_entries=settings.bq_total_entries)
 
 
 @app.route("/search_api", methods=['GET', 'POST'])
 def search_api():
-    setup_app()
+    error_msg = settings.pull_metadata()
+    if error_msg:
+        app.logger.error(f"[ERROR] {error_msg}")
     filtered_meta_data = []
-    if bq_table_files['bq_metadata']['file_data']:
-        filtered_meta_data = filter_rows(bq_table_files['bq_metadata']['file_data'], request)
+    try:
+        query_statement = bq_builder.metadata_query(request)
+        print(query_statement)
+        bigquery_client = bigquery.Client(project=settings.BQ_METADATA_PROJ)
+        query_job = bigquery_client.query(query_statement)
+
+        result = query_job.result(timeout=30)
+        # print(result.length)
+        # for row in result:
+        #     print(dict(row)['metadata'])
+        filtered_meta_data = [json.loads(dict(row)['metadata']) for row in result]
+        # print('len(result)')
+        # print(filtered_meta_data)
+        # filtered_meta_data = [dict(row) for row in result]
+    except ValueError:
+        error_msg = 'An invalid query parameter was detected. Please revise your search criteria and search again.'
+    except (concurrent.futures.TimeoutError, requests.exceptions.ReadTimeout):
+        error_msg = "Sorry, query job has timed out."
+    except (BadRequest, Exception) as e:
+        print(e)
+        error_msg = "There was an error during the download process."
+    if error_msg:
+        app.logger.error(f"[ERROR] {error_msg}")
     return jsonify(filtered_meta_data)
 
 
@@ -293,31 +296,40 @@ def filter_by_all_labels(rq_meth, rows):
     return rows
 
 
-def filter_rows(rows, req):
-    if req.method == 'POST':
-        rq_meth = req.form
-    else:
-        rq_meth = req.args
-    generic_filters = ['description', 'friendlyName']
-    tbl_ref_filters = ['projectId']
-    tbl_ref_filters2 = ['datasetId', 'tableId']
-    label_filters = ['status', 'category', 'experimental_strategy', 'data_type', 'source', 'program',
-                     'reference_genome']
 
-    filter_prop_list = [{'attr_list': generic_filters, 'subcategory': None, 'is_exact_match': False},
-                        {'attr_list': tbl_ref_filters, 'subcategory': 'tableReference', 'is_exact_match': True},
-                        {'attr_list': tbl_ref_filters2, 'subcategory': 'tableReference', 'is_exact_match': False},
-                        {'attr_list': label_filters, 'subcategory': 'labels', 'is_exact_match': True}]
-    for filter_prop in filter_prop_list:
-        rows = filter_by_prop(rq_meth, rows, filter_prop['attr_list'], filter_prop['subcategory'],
-                              filter_prop['is_exact_match'])
-    rows = filter_by_field_name(rq_meth, rows)
-    rows = filter_by_all_labels(rq_meth, rows)
-    if rq_meth.get('include_always_newest','').lower() == 'false':
-        rows = exclude_always_new(rows)
+# def filter_rows(req):
+#     if req.method == 'POST':
+#         rq_meth = req.form
+#     else:
+#         rq_meth = req.args
+#     filter_attrs = ['description', 'friendlyName', 'projectId', 'datasetId', 'tableId', 'status', 'category',
+#                     'experimental_strategy', 'data_type', 'source', 'program', 'reference_genome',
+#                     'include_always_newest']
+#     for attr in filter_attrs:
+#         f_vals = rq_meth.get(attr, '').strip().lower()
 
-    return rows
 
+#     generic_filters = ['description', 'friendlyName']
+#     tbl_ref_filters = ['projectId']
+#     tbl_ref_filters2 = ['datasetId', 'tableId']
+#     label_filters = ['status', 'category', 'experimental_strategy', 'data_type', 'source', 'program',
+#                      'reference_genome']
+#
+#     filter_prop_list = [{'attr_list': generic_filters, 'subcategory': None, 'is_exact_match': False},
+#                         {'attr_list': tbl_ref_filters, 'subcategory': 'tableReference', 'is_exact_match': True},
+#                         {'attr_list': tbl_ref_filters2, 'subcategory': 'tableReference', 'is_exact_match': False},
+#                         {'attr_list': label_filters, 'subcategory': 'labels', 'is_exact_match': True}]
+#     for filter_prop in filter_prop_list:
+#         rows = filter_by_prop(rq_meth, rows, filter_prop['attr_list'], filter_prop['subcategory'],
+#                               filter_prop['is_exact_match'])
+#     rows = filter_by_field_name(rq_meth, rows)
+#     rows = filter_by_all_labels(rq_meth, rows)
+#     if rq_meth.get('include_always_newest','').lower() == 'false':
+#         rows = exclude_always_new(rows)
+#
+#     return rows
+#     rows = []
+#     return rows
 
 def get_schema_fields(schema_field):
     nested_schema_dic = {}
@@ -332,86 +344,10 @@ def get_schema_fields(schema_field):
         return schema_field.name
 
 
-def setup_app():
-    global bq_table_files, bq_total_entries
-    status_code = 200
-    try:
-        is_bq_metadata_updated = False
-        is_useful_join_updated = False
-        is_version_file_updated = False
-        is_version_map_file_updated = False
-        for f in bq_table_files:
-            r = requests.head(bq_table_files[f]['file_path'] + '?t=' + str(randint(1000, 9999)))
-            r.raise_for_status()
-            file_last_modified = datetime.strptime(r.headers['Last-Modified'], '%a, %d %b %Y %H:%M:%S GMT')
-            if not bq_table_files[f]['file_data'] or \
-                    not bq_table_files[f]['last_modified'] or (
-                    bq_table_files[f]['last_modified'] and (bq_table_files[f]['last_modified'] < file_last_modified)):
-                bq_table_files[f]['last_modified'] = file_last_modified
-                bq_table_files[f]['file_data'] = requests.get(bq_table_files[f]['file_path']).json()
-                if f == 'bq_metadata':
-                    is_bq_metadata_updated = (not is_bq_metadata_updated)
-                elif f == 'bq_useful_join':
-                    is_useful_join_updated = not is_useful_join_updated
-                elif f == 'bq_versions':
-                    is_version_file_updated = not is_version_file_updated
-                elif f == 'bq_marked_table_map':
-                    is_version_map_file_updated = not is_version_map_file_updated
-        bq_total_entries = len(bq_table_files['bq_metadata']['file_data']) if bq_table_files['bq_metadata'][
-            'file_data'] else 0
-        if (is_bq_metadata_updated and bq_total_entries) or is_useful_join_updated or is_version_file_updated or is_version_map_file_updated:
-            for bq_meta_data_row in bq_table_files['bq_metadata']['file_data']:
-                useful_joins = []
-                row_id = bq_meta_data_row['id']
 
-                for join in bq_table_files['bq_useful_join']['file_data']:
-                    if join['id'] == row_id:
-                        useful_joins = join['joins']
-                        break
-                bq_meta_data_row['usefulJoins'] = useful_joins
-                table_version_info = None
-                if 'labels' in bq_meta_data_row and 'version' in bq_meta_data_row['labels']:
-                    labeled_version = bq_meta_data_row['labels']['version']
-                    split_ids = re.split(':|\.', row_id)
-                    proj_id = split_ids[0]
-                    tbl_ds_id = split_ids[1]
-                    tbl_tbl_id = split_ids[2]
-                    version_id = None
-                    if tbl_tbl_id.endswith('_current'):
-                        root_tbl_tbl_id = tbl_tbl_id.removesuffix('current')
-                        version_id = f'{proj_id}:{tbl_ds_id}.{root_tbl_tbl_id}'
-                    else:
-                        if bq_table_files['bq_marked_table_map']['file_data'] and tbl_ds_id in bq_table_files['bq_marked_table_map']['file_data'][proj_id]:
-                            for t in bq_table_files['bq_marked_table_map']['file_data'][proj_id][tbl_ds_id]:
-                                if (t.startswith('_') and tbl_tbl_id.endswith(t)) or (t.endswith('_') and tbl_tbl_id.startswith(t)):
-                                    version_id = bq_table_files['bq_marked_table_map']['file_data'][proj_id][tbl_ds_id][t]
-                                    break
-                        if not version_id and tbl_ds_id.endswith('_versioned'):
-                            root_tbl_ds_id = tbl_ds_id.removesuffix('_versioned')
-                            root_tbl_tbl_id = tbl_tbl_id.removesuffix(f'{labeled_version}'.lower())
-                            root_tbl_tbl_id = root_tbl_tbl_id.removesuffix(f'{labeled_version}'.upper())
-                            version_id = f'{proj_id}:{root_tbl_ds_id}.{root_tbl_tbl_id}'
-                    if version_id and version_id in bq_table_files['bq_versions']['file_data']:
-                        table_version_info = bq_table_files['bq_versions']['file_data'][version_id]
-                bq_meta_data_row['versions'] = table_version_info
-    except requests.exceptions.HTTPError as e:
-        error_message = 'HTTPError'
-        status_code = e.response.status_code
-    except requests.exceptions.ReadTimeout as e:
-        error_message = 'ReadTimeout'
-        status_code = e.response.status_code
-    except requests.exceptions.ConnectionError as e:
-        error_message = 'ConnectionError'
-        status_code = e.response.status_code
 
-    if status_code != 200:
-        bq_table_files['bq_filters']['file_data'] = None
-        bq_table_files['bq_metadata']['file_data'] = None
-        bq_table_files['bq_useful_join']['file_data'] = None
-        bq_total_entries = 0
-        app.logger.error(f'ERROR While attempting to retrieve BQ metadata file: [{status_code}] {error_message}')
-
-setup_app()
+# setup_app()
+settings.setup_app(app)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
