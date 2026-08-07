d7eb2db34159cf83105444009be41ac130113569
wlongabaugh <wlongabaugh@systemsbiology.org>
2026-08-06 17:12:26 -0700

    Debug return values II

--- main.py
+++ main.py
@@ -86,8 +86,8 @@ def search_api():
     filtered_meta_data = []
     try:
         query_statement, parameters, old_query = bq_builder.metadata_query(request)
-        result = bq_proxy.query_for_result(settings, parameters, query_statement, old_query)
-        filtered_meta_data = [json.loads(dict(row)['metadata']) for row in result]
+        filtered_meta_data = bq_proxy.query_for_result(settings, parameters, query_statement, old_query)
+        # filtered_meta_data = [json.loads(dict(row)['metadata']) for row in result]
 
     except Exception as e:
         status_code=400
